8a175b2225a5c91c992cb82fa87ffd0197027388
Fong Dai Goresmack <exastar@gmx.de>
2024-02-09 14:14:07 +0100

    Default Locale US

--- src/main/java/com/fx/w2p/Globals.java
+++ src/main/java/com/fx/w2p/Globals.java
@@ -1,6 +1,7 @@
 package com.fx.w2p;
 
 import java.nio.ByteOrder;
+import java.util.Locale;
 
 public class Globals {
 
@@ -24,6 +25,8 @@ public class Globals {
 
     //Debug Infos - Ein / Aus
     public static boolean DEBUG = true;
+
+    public static final Locale DEFAULT_LOCALE = Locale.US;
 
     //Show Test-UI (Managed UI Ein / Aus)
     public static boolean SHOW_TEST_UI = false;
--- src/main/java/com/fx/w2p/MainApp.java
+++ src/main/java/com/fx/w2p/MainApp.java
@@ -26,7 +26,7 @@ import org.apache.logging.log4j.core.LoggerContext;
 public class MainApp extends Application {
 
     private static final Logger _log = LogManager.getLogger(MainApp.class);
-    private final ResourceBundle bundle = ResourceBundle.getBundle(Globals.BUNDLE_PATH, Locale.getDefault());
+    private final ResourceBundle bundle = ResourceBundle.getBundle(Globals.BUNDLE_PATH, Globals.DEFAULT_LOCALE);
 
     @Override
     public void start(Stage stage) throws Exception {
